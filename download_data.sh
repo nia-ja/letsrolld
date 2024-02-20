e92689efda8e0b998b80e06c52a5967dd4e0974b
Ihar Hrachyshka <ihrachys@redhat.com>
2024-02-19 21:26:00 -0500

    Add -o to unzip to force overwriting existing data files

--- download_data.sh
+++ download_data.sh
@@ -13,5 +13,5 @@ curl 'https://letterboxd.com/data/export/' \
   -H 'x-requested-with: XMLHttpRequest' \
   --compressed > $DIR/letterboxd-export.zip
 
-unzip $DIR/letterboxd-export.zip -d $DIR/letterboxd-export
+unzip -o $DIR/letterboxd-export.zip -d $DIR/letterboxd-export
 cp $DIR/letterboxd-export/watched.csv .
