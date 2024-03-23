0a234e6a74cb11e3ef107eaf2eb889657176cd83
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-03-23 09:47:43 -0400

    remove dependency for requests_cache

--- src/letsrolld/http.py
+++ src/letsrolld/http.py
@@ -2,7 +2,6 @@ import logging
 from http.client import HTTPConnection
 
 import requests
-import requests_cache as rc
 
 
 _CACHE_INSTALLED = False
@@ -20,49 +19,15 @@ def enable_debug():
     requests_log.propagate = True
 
 
-# TODO: configure different expire times for different entities
-# e.g. update directors once a month in case new movies are released
-# but never movies themselves (except justwatch availability info
-# that is more dynamic)
-def _install_cache():
-    global _CACHE_INSTALLED
-    if not _CACHE_INSTALLED:
-        # TODO: expire the cache after a certain time
-        rc.install_cache("cache", allowable_methods=("GET", "HEAD", "POST"))
-        _CACHE_INSTALLED = True
-
-
 def get_url(url):
-    # _install_cache()
-
-    def _get_url(url, entity):
-        return entity.get(url).text
-
-    try:
-        return _get_url(url, requests)
-    except Exception as e:
-        print(e, url)
-        return _get_url(
-            url, rc.CachedSession(expire_after=rc.EXPIRE_IMMEDIATELY)
-        )
+    return requests.get(url).text
 
 
 def get_json(url, json, validator=None):
-    # _install_cache()
-
     def _get_json(url, json, entity, validator):
         res = entity.post(url, json=json).json()
         if validator is not None and not validator(res):
             raise ValueError("Invalid response")
         return res
 
-    try:
-        return _get_json(url, json, requests, validator=validator)
-    except Exception as e:
-        print(e, json)
-        return _get_json(
-            url,
-            json,
-            rc.CachedSession(expire_after=rc.EXPIRE_IMMEDIATELY),
-            validator=None,
-        )
+    return _get_json(url, json, requests, validator=validator)
