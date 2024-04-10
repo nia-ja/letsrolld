ab3d43744d502eef8105b7ddcb978b6afbe1164c
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-04-09 21:57:43 -0400

    Normalize lb_urls by removing www. prefix
    This is redirect, and fetching it directly wastes requests.

--- src/letsrolld/cmd/cleanup.py
+++ src/letsrolld/cmd/cleanup.py
@@ -6,6 +6,38 @@ from letsrolld import db
 from letsrolld.db import models
 
 
+_REDIRECT_LB_PREFIX = "https://www.letterboxd.com"
+_NORMALIZED_LB_PREFIX = _REDIRECT_LB_PREFIX.replace("www.", "")
+
+
+def lb_obj_exists(session, model, lb_url):
+    q = session.query(model).filter(model.lb_url == lb_url)
+    return session.query(q.exists()).scalar()
+
+
+def normalize_lb_url(lb_url):
+    return lb_url.replace(_REDIRECT_LB_PREFIX, _NORMALIZED_LB_PREFIX)
+
+
+def shorten_lb_urls(session, model, dry_run=False):
+    try:
+        for obj in session.query(model).filter(
+                model.lb_url.startswith(_REDIRECT_LB_PREFIX)).all():
+            new_url = normalize_lb_url(obj.lb_url)
+            if lb_obj_exists(session, model, new_url):
+                print(f"Deleting duplicate {obj.lb_url}")
+                session.delete(obj)
+            else:
+                print(f"Updating lb_url for {obj.lb_url}")
+                obj.lb_url = new_url
+                session.add(obj)
+    finally:
+        if not dry_run:
+            session.commit()
+        else:
+            session.rollback()
+
+
 def delete_orphaned_films(session, model, dry_run=False):
     try:
         for film in session.query(model).all():
@@ -20,6 +52,14 @@ def delete_orphaned_films(session, model, dry_run=False):
 
 
 _CLEANUP = [
+    (
+        models.Director,
+        shorten_lb_urls,
+    ),
+    (
+        models.Film,
+        shorten_lb_urls,
+    ),
     (
         models.Film,
         delete_orphaned_films,
