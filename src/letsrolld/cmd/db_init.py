10d5b1a7f0ed9ff34cd57c1e54ec5ce61d564413
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-04-21 16:45:57 -0400

    Fix db_init - missing argument

--- src/letsrolld/cmd/db_init.py
+++ src/letsrolld/cmd/db_init.py
@@ -1,6 +1,7 @@
 from letsrolld import db
+from letsrolld.db import models
 
 
 def main():
     engine = db.create_engine()
-    db.create_tables(engine)
+    db.create_tables(models.Base.metadata, engine)
