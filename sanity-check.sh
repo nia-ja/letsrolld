4c3dc76278a142ded1863acdfedd0b30efa6ee8a
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-05-24 18:36:51 -0400

    sanity-check: validate webapp / webcli working

--- sanity-check.sh
+++ sanity-check.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
+set -xe
 
 DIRECTORS_FILE=directors.csv
 
+# create empty database
 alembic upgrade head
 
+# populate database with some data
 populate-directors -d ${DIRECTORS_FILE} -n 2
 update-directors
 update-films
 update-offers
-
 cleanup
 
-# TODO: check that the db has some records
+# start webapp
+webapp &
+WEBAPP_PID=$!
+sleep 5
+
+# check that it is running and returns some data
+lines=$(lcli films get | wc -l)
+test $lines -eq 10  # 10 is default in webapi
+
+# we know which directors we fed into the database
+# (the first two entries in the input file)
+out=$(lcli directors get)
+# TODO: we could probably extract these programmatically here
+test grep -q "Maryam Touzani" $out
+test grep -q "Štefan Uher" $out
+
+# stop webapp
+kill $WEBAPP_PID
