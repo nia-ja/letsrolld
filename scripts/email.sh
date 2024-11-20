b891c987a988837118b293e9c632da3514fda308
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-11-20 18:02:52 -0500

    Minor report rendering adjustments

--- scripts/email.sh
+++ scripts/email.sh
@@ -3,14 +3,12 @@
 FROMADDR=ihar.hrachyshka@gmail.com
 TOADDRS=$FROMADDR
 
-REPORTFILE=movies.report
-
 tmpdir=$(mktemp -d)
 reportfile=$tmpdir/movies.report
 
-lcli report render --name default > $reportfile
+lcli report render --name default > "$reportfile"
 mailsend-go -from $FROMADDR -t $TOADDRS -sub "Movies for $(date '+%Y-%m-%d')" \
-  -use gmail auth -user $FROMADDR -pass $(pass priv/google.com-mutt) \
-  body -file $reportfile
+  -use gmail auth -user $FROMADDR -pass "$(pass priv/google.com-mutt)" \
+  body -file "$reportfile"
 
-rm -r $tmpdir
+rm -r "$tmpdir"
