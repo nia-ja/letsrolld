fb1e3f786faf6cfd41b9f00281a465b3f7c1b599
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-12-05 10:16:11 -0500

    Separate script to generate report and to send it via email

--- scripts/email.sh
+++ scripts/email.sh
@@ -2,20 +2,13 @@
 
 set -e
 
-# Accept a parameter --start-server to start the server before sending the email.
-if [ "$1" = "--start-server" ]; then
-  flox activate -- make webapp &
-  trap 'kill %1' EXIT
-  sleep 10 # Wait for the server to start.
-fi
-
 FROMADDR=ihar.hrachyshka@gmail.com
 TOADDRS=$FROMADDR
 
 tmpdir=$(mktemp -d)
 reportfile=$tmpdir/movies.report
+./scripts/gen_report.sh -s "$reportfile"
 
-flox activate -- lcli report render --name default > "$reportfile"
 mailsend-go -from $FROMADDR -t $TOADDRS -sub "Movies for $(date '+%Y-%m-%d')" \
   -use gmail auth -user $FROMADDR -pass "$(pass priv/google.com-mutt)" \
   body -file "$reportfile"
